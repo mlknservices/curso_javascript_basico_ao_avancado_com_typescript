1dcb6e4a6b9b201bbb05387d0bb5408040c738c1
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-19 20:40:16 -0300

    C05 - 2. JavaScript Básico - Para iniciantes; 6. Variáveis com let

---
+++ section2/06/6_variaveis_com_let.js
@@ -0,0 +1,7 @@
+let nome = 'Marcelo';
+
+console.log(nome, 'nasceu em 1979');
+console.log('Em 2003', nome, 'conheceu Aline.');
+console.log(nome, 'casou-se com Aline em 2005.');
+console.log('Aline teve um filho com', nome, 'em 2005.');
+console.log('O filho de', nome, 'se chama Caio.');
