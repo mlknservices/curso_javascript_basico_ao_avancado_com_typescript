9ca315ecfad6137c52e7083bddb1ae921b8f88e0
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-19 20:33:34 -0300

    C01 - 2. JavaScript Básico - Para iniciantes; 1. Console.log

---
+++ section2/01/1_console_log.js
@@ -0,0 +1,4 @@
+console.log("Marcelo Marcondes");
+console.log("Caio Soares 'Marcondes' de Oliveira");
+console.log('Ana Clara Soares "Marcondes" de Oliveira');
+console.log(123, 45.67, "Marcelo Marcondes de Oliveira Santos");
