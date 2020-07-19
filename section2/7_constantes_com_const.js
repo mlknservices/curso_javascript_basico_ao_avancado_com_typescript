53336327075b7405ebeebc45d038430265ac2458
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-19 20:41:28 -0300

    C06 - 2. JavaScript Básico - Para iniciantes; 7. Constantes com const

---
+++ section2/7_constantes_com_const.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+
+console.log(resultado);
+
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultadoDuplicado);
