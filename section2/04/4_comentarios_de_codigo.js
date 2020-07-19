7dba8f7f73534c23096a29f13e7122b499c12fdc
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-19 20:36:18 -0300

    C03 - 2. JavaScript Básico - Para iniciantes; 4. Comentários de código

---
+++ section2/04/4_comentarios_de_codigo.js
@@ -0,0 +1,24 @@
+// Comentário de linha
+
+// Um comentário
+console.log("Olá mundo!!!") //Aqui tem outro comentário
+console.log("Linha 1")
+
+
+/*   */ // Comentário de bloco
+
+/* 
+ * Um título
+ *
+ *Lorem ipsum
+ *
+ */
+
+// console.log("Linha 2")
+
+/*
+console.log("Linha 3")
+console.log("Linha 4")
+*/
+
+//Comentário ignorado
