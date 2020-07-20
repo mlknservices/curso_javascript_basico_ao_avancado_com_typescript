a65aa7792d3cfbf6248c37ae31dc5e0e8a5ead90
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-20 16:33:29 -0300

    C07 - 2. JavaScript Básico - Para iniciantes; 9. Exercícios - Const e Let

---
+++ section2/09/9_exercicios_const_e_let.js
@@ -0,0 +1,44 @@
+const nome = 'Marcelo';
+const sobreNome = 'Marcondes de Oliveira Santos';
+const idade = 41;
+const peso = 158;
+const altura = 1.98;
+let imc = '';
+let anoNascimento = '';
+
+imc = peso / (altura * altura);
+
+anoNascimento = 2020 - idade;
+
+var imcIndice = '';
+
+if (imc > 0 && imc < 18.5) {
+  imcIndice = 'você está abaixo do peso ideal';
+} else if (imc >= 18.5 && imc < 25) {
+  imcIndice = 'você está em seu peso normal';
+} else if (imc >= 25 && imc < 30) {
+  imcIndice = 'você está acima de seu peso (sobrepeso)';
+} else if (imc >= 30 && imc < 35) {
+  imcIndice = 'obesidade grau I';
+} else if (imc >= 35 && imc < 40) {
+  imcIndice = 'obesidade grau II';
+} else if (imc <= 0) {
+  imcIndice = 'cálculo incorreto, reveja seus dados inseridos';
+} else {
+  imcIndice = ('obesidade grau III');
+}
+
+console.log(nome, sobreNome, 'tem', idade, 'anos, pesa', peso, 'Kg.');
+console.log(nome, 'nasceu em', anoNascimento, '.');
+console.log(nome, 'tem', altura, 'de altura e seu IMC é', imc, '.');
+console.log(nome, ', o cálculo do seu IMC é classificado como "', imcIndice, '".');
+console.log();
+console.log(nome + ' ' + sobreNome + ' tem ' + idade + ' anos, pesa ' + peso + ' Kg.');
+console.log(nome + ' nasceu em ' + anoNascimento + '.');
+console.log(nome + ' tem ' + altura + ' de altura e seu IMC é ' + imc + '.');
+console.log(nome + ', o cálculo do seu IMC é classificado como "' + imcIndice + '".');
+console.log();
+console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso} Kg.`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
+console.log(`${nome} tem ${altura} de altura e seu IMC é ${imc}.`);
+console.log(`${nome}, o cálculo do seu IMC é classificado como ${imcIndice}.`);
