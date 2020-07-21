15e2cca83322d7f4ea4f7b79a3ac58275cee03fa
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-21 00:24:34 -0300

    C08 - 2. JavaScript Básico - Para iniciantes; 11. Let vs Var - Primeira diferença

---
+++ section2/11/11_let_vs_var_primeira_diferenca.js
@@ -0,0 +1,15 @@
+/* Váriáveis criadas com 'var' podem ter seus valores redeclarados */
+
+var nome = 'Marcelo';
+
+var nome = 'Caio';
+
+console.log(nome);
+
+/* Váriáveis criadas com 'let' não podem ter seus valores redeclarados */
+
+let sobreNome = 'Marcondes';
+
+let sobreNome = 'Oliveira';
+
+console.log(sobreNome);
