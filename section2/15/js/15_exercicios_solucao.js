1a54f52bbb36913cdd6700d184ba2a14bd2d8154
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-21 23:55:07 -0300

    C12 - 2. JavaScript Básico - Para iniciantes; 15. Exercícios - Solução

---
+++ section2/15/js/15_exercicios_solucao.js
@@ -0,0 +1,12 @@
+let num1 = prompt('Digite um número:');
+
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const calc = num1 + num2;
+
+alert(`O resultado da soma dos dois números informados é ${calc}`);
+
+alert(`O resultado da soma dos dois números informados é ${num1 - num2}`);
