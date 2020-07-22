723f8efd2e16c1333e0482370c9119f817840c66
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-07-21 23:34:10 -0300

    C11 - 2. JavaScript Básico - Para iniciantes; 14. alert, confirm e prompt (Navegador)

---
+++ section2/14/js/14_alert_confirm_e_prompt_Navegador.js
@@ -0,0 +1,9 @@
+// Alertas de exibição no navegador
+
+alert('Teste de alerta');
+
+window.alert('Outro teste de alerta');
+
+window.confirm('Deseja realmente cancelar?');
+
+window.prompt('Digite seu nome:');
