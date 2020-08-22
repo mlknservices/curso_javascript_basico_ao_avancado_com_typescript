5077574e839b905232dd63b14f3dda0d2416d44d
Marcelo MLKN <marcelomarcondesos@gmail.com>
2020-08-22 19:23:43 -0300

    C24 - 3. JavaScript - Lógica de programação; 2. Operadores de comparação

---
+++ section3/02/2_peradores_de_comparacao.js
@@ -0,0 +1,58 @@
+// * Maior que ">"
+
+const comp = 10 > 5;
+console.log(comp);
+
+
+// * Maior que ou igual ">="
+
+const comp2 = 10 >= 10;
+console.log(comp2);
+
+
+// * Menor que "<"
+
+const comp3 = 10 < 5;
+console.log(comp3);
+
+
+// * Menor que ou igual "<="
+
+const comp4 = 10 <= 11;
+console.log(comp4);
+
+
+// * Igual a "==" (verifica somente o valor)
+
+const comp5 = 10;
+const comp6 = '10';
+
+const comp7 = comp5 == comp6;
+console.log(comp7);
+
+
+// * Igual a "===" (verifica valor e tipo)
+
+const comp8 = 10;
+const comp9 = '10';
+
+const comp10 = comp8 === comp9;
+console.log(comp10);
+
+
+// * Deferente de "!==" (verifica somente o valor)
+
+const comp11 = 25;
+const comp12 = '25';
+
+const comp13 = comp11 != comp12;
+console.log(comp13);
+
+
+// * Diferente de "!===" (verifica valor e tipo)
+
+const comp14 = 25;
+const comp15 = '25';
+
+const comp16 = comp14 !== comp15;
+console.log(comp16);
